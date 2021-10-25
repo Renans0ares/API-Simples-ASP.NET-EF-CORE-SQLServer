0e8efaeca143026e7066912ea34dc2a5087b71ad
renan <renan@RENANMACHINE>
2021-10-25 17:52:45 -0300

    Add project files.

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MeuTodo.Models;
+using MeuTodo.Data;
+using Microsoft.EntityFrameworkCore;
+using MeuTodo.ViewModels;
+
+namespace MeuTodo.Controllers
+{
+    [ApiController]
+    [Route(template: "v1")]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet]
+        [Route(template: "todos")]
+        public async Task<IActionResult> GetAsync(
+            [FromServices] AppDbContext context)
+        {
+            var todos = await context.Todos.AsNoTracking().ToListAsync();
+            return Ok(todos);
+        }
+
+        [HttpGet]
+        [Route(template: "todos/{id}")]
+        public async Task<IActionResult> GetByIdAsync([FromServices] AppDbContext context, [FromRoute] int id)
+        {
+            var todo = await context.Todos.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            return todo == null ? NotFound() : Ok();
+        }
+
+        [HttpPost(template: "todos")]
+        public async Task<IActionResult> PostAsync([FromServices] AppDbContext context, [FromBody] CreateTodoViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var todo = new Todo
+            {
+                Date = DateTime.Now,
+                Done = false,
+                Title = model.Title
+            };
+
+            try
+            {
+                await context.Todos.AddAsync(todo);
+                await context.SaveChangesAsync();
+                return Created(uri: $"v1/todos/{todo.Id}", todo);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut(template: "todos/{id}")]
+        public async Task<IActionResult> PutAsync([FromServices] AppDbContext context, [FromBody] CreateTodoViewModel model, [FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var todo = await context.Todos.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (todo == null)
+            {
+                NotFound();
+            }
+
+            try
+            {
+                todo.Title = model.Title;
+
+                context.Todos.Update(todo);
+                await context.SaveChangesAsync();
+
+                return Ok(todo);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete(template: "todos/{id}")]
+        public async Task<IActionResult> DeleteAsync([FromServices] AppDbContext context, [FromRoute] int id)
+        {
+            var todo = await context.Todos.FirstOrDefaultAsync(x => x.Id == id);
+
+            try
+            {
+                context.Todos.Remove(todo);
+                await context.SaveChangesAsync();
+                return Ok("deletado");
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MeuTodo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeuTodo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=CrudTodo");
+    }
+}
